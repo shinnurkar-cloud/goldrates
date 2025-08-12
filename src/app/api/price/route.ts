05a0ebfed9777344621b1c95aa87b9ea217bfdce
Anilkumar shinnurkar <shinnurkar@gmail.com>
2025-08-12 18:26:18 +0000

    Get API Key

--- src/app/api/price/route.ts
+++ src/app/api/price/route.ts
@@ -1,9 +1,24 @@
 import { NextResponse } from 'next/server';
 import { getGoldPrice } from '@/lib/data';
+import { headers } from 'next/headers';
 
 export const dynamic = 'force-dynamic';
 
-export async function GET() {
+export async function GET(request: Request) {
+  const headersList = headers();
+  const authHeader = headersList.get('Authorization');
+  const apiKey = process.env.GOLD_API_KEY;
+
+  if (!apiKey) {
+    return NextResponse.json({ message: 'API key is not configured on the server.' }, { status: 500 });
+  }
+
+  const expectedAuthHeader = `Bearer ${apiKey}`;
+
+  if (!authHeader || authHeader !== expectedAuthHeader) {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }
+
   try {
     const goldPriceData = getGoldPrice();
     return NextResponse.json(goldPriceData);
