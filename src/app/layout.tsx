793c803b7cbc0d82f6622e5f8626f173bbb40816
Anilkumar shinnurkar <shinnurkar@gmail.com>
2025-08-13 09:38:54 +0000

    some golden color orange color type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content="#7c3aed" />
+        <meta name="theme-color" content="#f0a540" />
       </head>
       <body className="font-body antialiased">
         {children}
