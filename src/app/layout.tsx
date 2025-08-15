67f822dcd01335671aaf22ff19807d3aff375891
Anilkumar shinnurkar <shinnurkar@gmail.com>
2025-08-15 14:08:10 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h1 class="text-4x

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter&family=Teko:wght@700&display=swap" rel="stylesheet" />
         <meta name="theme-color" content="#f0a540" />
       </head>
       <body className="font-body antialiased">
